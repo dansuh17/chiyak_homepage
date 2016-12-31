9ce834c563b64d6847f91efa4fea62839db27b48
DongwooSuh <kehops@kaist.ac.kr>
2017-01-01 01:04:48 +0900

    Added testing dependency.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies, no-undef, react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
+// do a shallow rendering to eliminate store dependency
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
