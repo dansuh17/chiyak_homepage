6081098c7b0b0a88025e016a9abf615a0ca60e3e
DongwooSuh <kehops@kaist.ac.kr>
2017-01-27 00:47:35 +0900

    Make use of Bootstrap responsive features.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Grid, Nav, NavItem, Navbar } from 'react-bootstrap';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import logoimg from '../img/chiyakLogo.png';
 import './Header.css';
 
 const Header = () => (
-  <Navbar bsStyle="default" fixedTop>
-    <Grid>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/"><img className="logoimg" src={logoimg} alt="#" /></a>
-        </Navbar.Brand>
-        <Nav>
-          {/* Contains the link to each pages */}
-          <NavItem eventKey={1} href="/about">About</NavItem>
-          <NavItem eventKey={2} href="/news">새소식</NavItem>
-          <NavItem eventKey={3} href="/product">Product</NavItem>
-          <NavItem eventKey={4} href="/contact">Contact</NavItem>
-        </Nav>
-      </Navbar.Header>
-    </Grid>
+  <Navbar bsStyle="default" fixedTop collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/"><img className="logoimg" src={logoimg} alt="#" /></a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        {/* Contains the link to each pages */}
+        <NavItem eventKey={1} href="/about">About</NavItem>
+        <NavItem eventKey={2} href="/news">새소식</NavItem>
+        <NavItem eventKey={3} href="/product">Product</NavItem>
+        <NavItem eventKey={4} href="/contact">Contact</NavItem>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,31 +1,85 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Col, Row, Grid, Jumbotron } from 'react-bootstrap';
 import logo from '../logo.svg';
 import './Home.css';
 
 const Home = () => (
   <div>
-    <Jumbotron>
-      <div className="col12 MainDiv">
-        <h2 className="col5 TitleCaption">좋은치약 홈페이지</h2>
-      </div>
-    </Jumbotron>
+    <div className="col12 MainDiv">
+      <h2 className="col5 TitleCaption">좋은치약 홈페이지</h2>
+    </div>
     <div className="App-header">
       {/* spinning logo */}
       <img src={logo} className="App-logo" alt="logo" />
     </div>
-    <p>
-      치약 사진 + 짧은 글
-    </p>
-    <p>
-      Team 사진 + 짧은 글
-    </p>
-    <p>
-      구매처 + 짧은 글
-    </p>
-    <p>
+    <h2 className="home-subtitle1">좋은 치약</h2>
+    <Row className="row-padding">
+      <Col className="home-subsection" sm={12} md={3}>
+        <h6>글 하나</h6>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaeca tcupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </div>
+      </Col>
+      <Col className="home-subsection" sm={12} md={3}>
+        <h6>글 둘</h6>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaeca tcupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </div>
+      </Col>
+      <Col className="home-subsection" sm={12} md={3}>
+        <h6>글 셋</h6>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaeca tcupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </div>
+      </Col>
+      <Col className="home-subsection" sm={12} md={3}>
+        <h6>글 넷</h6>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaeca tcupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </div>
+      </Col>
+    </Row>
+    <Grid>
+      <Row className="row-padding">
+        <Jumbotron>
+          Team 사진 + 짧은 글
+        </Jumbotron>
+      </Row>
+    </Grid>
+    <Grid>
+      <Row className="row-padding">
+        <Col md={12} sm={12}>
+          구매처 + 짧은 글
+        </Col>
+      </Row>
+    </Grid>
+    <Grid>
       오프라인 구매처
-    </p>
+    </Grid>
   </div>
 );
 
