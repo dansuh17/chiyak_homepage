89af48eaa251bea7b7bd702a7f43143dd4b67fe7
DongwooSuh <kehops@kaist.ac.kr>
2017-02-05 20:33:00 +0900

    Center Align

--- client/src/components/Menubar.jsx
+++ client/src/components/Menubar.jsx
@@ -9,11 +9,11 @@ const Menubar = () => (
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse className="nav-collapse-container">
-      <Nav>
+      <Nav className="nav-center">
         {/* Contains the link to each pages */}
         <NavItem eventKey={1}><Link to="/About">About</Link></NavItem>
         <NavItem eventKey={2}><Link to="/News">새소식</Link></NavItem>
-        <NavItem eventKey={3}><Link to="/Products">Product</Link></NavItem>
+        <NavItem eventKey={3}><Link to="/Product">Product</Link></NavItem>
         <NavItem eventKey={4}><Link to="/Contact">Contact</Link></NavItem>
       </Nav>
     </Navbar.Collapse>
