355f93bca510b0f9ca95c04caf083f4742064730
Yeon-joo Oh <ohMyGodBlessYou@Yeon-jooui-MacBook-Air.local>
2017-02-09 19:13:29 +0900

    "menubar test"

--- client/src/components/Menubar.jsx
+++ client/src/components/Menubar.jsx
@@ -1,23 +1,25 @@
-import { Link } from 'react-router';
 import React from 'react';
-import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import './Menubar.css';
+import Scroller from './Scroller';
 
 const Menubar = () => (
-  <Navbar bsStyle="default" className="header2" fixedTop collapseOnSelect>
+  <div>
+    <Scroller />
+  </div>
+  /* <Navbar bsStyle="default" className="header2" fixedTop collapseOnSelect>
     <Navbar.Header>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse className="nav-collapse-container">
       <Nav className="nav-center">
-        {/* Contains the link to each pages */}
+        {Contains the link to each pages }
         <NavItem eventKey={1}><Link to="/About">About</Link></NavItem>
         <NavItem eventKey={2}><Link to="/News">새소식</Link></NavItem>
-        <NavItem eventKey={3}><Link to="/Product">Product</Link></NavItem>
+        <NavItem eventKey={3}><Link to="/`Product">Product</Link></NavItem>
         <NavItem eventKey={4}><Link to="/Contact">Contact</Link></NavItem>
       </Nav>
     </Navbar.Collapse>
-  </Navbar>
+  </Navbar> */
 );
 
 export default Menubar;
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Scroller from './Scroller';
 
 const Products = () => (
   <div>
-    <Scroller />
     <p>제품 사진 + 링크</p>
     <p>링크 들어가면 설명 / 가격 등 볼 수 있음 - Material-UI card 등을 활용하면 될듯</p>
   </div>
