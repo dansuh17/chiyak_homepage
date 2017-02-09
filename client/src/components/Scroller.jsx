a652c15d766e4e854b636114aa75b628dcb53caa
DongwooSuh <kehops@kaist.ac.kr>
2017-02-10 02:06:18 +0900

    He net da chin gu ya!!

--- client/src/components/Scroller.jsx
+++ client/src/components/Scroller.jsx
@@ -9,6 +9,15 @@ class Scroller extends Component {
     Events.scrollEvent.remove('end');
   }
 
+  constructor(props) {
+    super(props);
+
+    // height offset - should be equal to height_header + height_scroll_header
+    this.state = {
+      offsetHeight: -100,
+    };
+  }
+
   componentDidMount() {
     Events.scrollEvent.register('begin', (...args) => {
       console.log('begin', args);  // eslint-disable-line no-console
@@ -24,90 +33,65 @@ class Scroller extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-default navbar-fixed-top-2">
-          <div>
-            <div>
-              <ul className="nav navbar-nav">
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="test1"
-                    to="test1"
-                    spy
-                    smooth
-                    duration={550}
-                  >
-                    Test 1
-                  </Link>
-                </li>
-                <li>
-                  <Link activeClass="active" className="test2" to="test2" spy smooth duration={500}>
-                    Test 2
-                  </Link>
-                </li>
-                <li>
-                  <Link activeClass="active" className="test3" to="test3" spy smooth duration={500}>
-                    Test 3
-                  </Link>
-                </li>
-                <li>
-                  <Link activeClass="active" className="test4" to="test4" spy smooth duration={500}>
-                    Test 4
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="test5"
-                    to="test5"
-                    spy
-                    smooth
-                    duration={500}
-                    delay={1000}
-                  >
-                    Test 5 ( delay )
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="test6"
-                    to="anchor"
-                    spy
-                    smooth
-                    duration={500}
-                  >
-                    Test 6 (anchor)
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <nav className="scrollnav">
+          <Link
+            activeClass="active"
+            className="scrollnav-item"
+            to="test1"
+            spy
+            smooth
+            duration={550}
+            offset={this.state.offsetHeight}
+          >
+            Test 1
+          </Link>
+          <Link
+            activeClass="active"
+            className="scrollnav-item"
+            to="test2"
+            spy
+            smooth
+            duration={500}
+            offset={this.state.offsetHeight}
+          >
+            Test 2
+          </Link>
+          <Link
+            activeClass="active"
+            className="scrollnav-item"
+            to="test3"
+            spy
+            smooth
+            duration={500}
+            offset={this.state.offsetHeight}
+          >
+            Test 3
+          </Link>
+          <Link
+            activeClass="active"
+            className="scrollnav-item"
+            to="test4"
+            spy
+            smooth
+            duration={500}
+            offset={this.state.offsetHeight}
+          >
+            Test 4
+          </Link>
         </nav>
 
         <Element name="test1" className="element" >
           test 1
         </Element>
-
         <Element name="test2" className="element">
           test 2
         </Element>
-
         <Element name="test3" className="element">
           test 3
         </Element>
-
         <Element name="test4" className="element">
           test 4
         </Element>
-
-        <Element name="test5" className="element">
-          test 5
-        </Element>
-
-        <div id="anchor" className="element">
-          test 6 (anchor)
-        </div>
       </div>
     );
   }
