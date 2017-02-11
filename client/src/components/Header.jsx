d6ee40925d46e98afdb1320d7520f437e77a609c
julie-oh <yj_oh@naver.com>
2017-02-11 16:25:40 +0900

    ha it

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import logoimg from '../img/chiyakLogo.png';
 import './Header.css';
 
 const Header = () => (
-  <Navbar bsStyle="default" className="header" fixedTop collapseOnSelect>
+  <Navbar bsStyle="default" className="header" fixedTop>
     <Navbar.Header>
       <Navbar.Brand>
         <a href="/"><img className="logoimg" src={logoimg} alt="#" /></a>
