17b309064e52c559fb60d7b42715bc42f15660b1
julie-oh <yj_oh@naver.com>
2017-02-21 18:11:41 +0900

    bless

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,7 @@ import './Home.css';
 const Home = () => (
   <div>
     <div className="col12 MainDiv">
-      <h1 className="col5 TitleCaption">좋은치약 홈페이지</h1>
+      <h1 className="col5 TitleCaption">From Mouth To Life:FMTL</h1>
     </div>
     <h1 className="home-subtitle1">
       {/* Go to another page */}
@@ -32,6 +32,20 @@ const Home = () => (
         </div>
       </Col>
     </Row>
+    <div className="sls">
+      <div className="jum">
+        <h3>합성계면활성제(SLS)란?</h3>
+        <p>치약에서 거품을 내는 성분으로 값싼 석유계 합성 화합물입니다</p>
+        <p>클렌저, 샴푸, 주방세제 등 대부분의 생활용품에 포함되어 있으며</p>
+        <p>최근에는 아토피 피부염 탈모 등의 문제로 점점 퇴출되고 있습니다</p>
+        <p>칫솔질 후 과일을 먹으면 쓰고 맛이 없는 이유는 합성계면 할성제가</p>
+        <p>미세포를 혼란시켜 미각이 일시적으로 둔화되기 떄문입니다</p>
+        <p>또한 입병을 유발하거나 악화시키고, 연약한 입안을 자극하여</p>
+        <p>칫솔질 할 떄 통증으로 구강관리를 소홀하게 합니다</p>
+        <p>그리고 입안을 마르고 텁텁하게 하여</p>
+        <p>오히려 입냄새가 증가하며 최소 10회 이상 헹궈내야 합니다</p>
+      </div>
+    </div>
   </div>
 );
 
