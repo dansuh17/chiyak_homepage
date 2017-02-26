4d5caa8086cb969b9b9da827c77ab0cc050da421
DongwooSuh <kehops@kaist.ac.kr>
2017-02-26 18:23:25 +0900

    orange juice 222

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,50 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, OverlayTrigger, Tooltip, Popover } from 'react-bootstrap';
+import questionMark from '../img/question_mark.png';
+import leaf from '../img/leaf.png';
+import smile from '../img/smile.png';
+import teeth from '../img/teeth.png';
+import flower from '../img/flower.png';
+import plus from '../img/plus.png';
 import './Home.css';
+
+const popoverHoverFocus = (
+  <Popover id="popover-trigger-hover-focus" title="합성계면활성제란(SLS)?">
+    <p>치약에서 거품을 내는 성분으로 값싼 석유계 합성 화합물입니다</p>
+    <p>클렌저, 샴푸, 주방세제 등 대부분의 생활용품에 포함되어 있으며</p>
+    <p>최근에는 아토피 피부염 탈모 등의 문제로 점점 퇴출되고 있습니다</p>
+    <p>칫솔질 후 과일을 먹으면 쓰고 맛이 없는 이유는 합성계면 할성제가</p>
+    <p>미세포를 혼란시켜 미각이 일시적으로 둔화되기 떄문입니다</p>
+    <p>또한 입병을 유발하거나 악화시키고, 연약한 입안을 자극하여</p>
+    <p>칫솔질 할 떄 통증으로 구강관리를 소홀하게 합니다</p>
+    <p>그리고 입안을 마르고 텁텁하게 하여</p>
+    <p>오히려 입냄새가 증가하며 최소 10회 이상 헹궈내야 합니다</p>
+  </Popover>
+);
+
+const tooltip1 = (
+  <Tooltip id="tooltip1">
+    <p>칫솔질 후에도 과일 맛이 그대로 누구나 안심하고 쓸 수 있는 자연거품</p>
+  </Tooltip>
+);
+
+const tooltip2 = (
+  <Tooltip id="tooltip2">
+    <p>칫솔질 후에도 과일 맛이 그대로 누구나 안심하고 쓸 수 있는 자연거품</p>
+  </Tooltip>
+);
+
+const tooltip3 = (
+  <Tooltip id="tooltip3">
+    <p>칫솔질 후에도 과일 맛이 그대로 누구나 안심하고 쓸 수 있는 자연거품</p>
+  </Tooltip>
+);
+
+const tooltip4 = (
+  <Tooltip id="tooltip4">
+    <p>칫솔질 후에도 과일 맛이 그대로 누구나 안심하고 쓸 수 있는 자연거품</p>
+  </Tooltip>
+);
 
 const Home = () => (
   <div>
@@ -30,20 +74,53 @@ const Home = () => (
         </div>
       </Col>
     </Row>
-    <div className="sls">
-      <div className="jum">
-        <h3>합성계면활성제(SLS)란?</h3>
-        <p>치약에서 거품을 내는 성분으로 값싼 석유계 합성 화합물입니다</p>
-        <p>클렌저, 샴푸, 주방세제 등 대부분의 생활용품에 포함되어 있으며</p>
-        <p>최근에는 아토피 피부염 탈모 등의 문제로 점점 퇴출되고 있습니다</p>
-        <p>칫솔질 후 과일을 먹으면 쓰고 맛이 없는 이유는 합성계면 할성제가</p>
-        <p>미세포를 혼란시켜 미각이 일시적으로 둔화되기 떄문입니다</p>
-        <p>또한 입병을 유발하거나 악화시키고, 연약한 입안을 자극하여</p>
-        <p>칫솔질 할 떄 통증으로 구강관리를 소홀하게 합니다</p>
-        <p>그리고 입안을 마르고 텁텁하게 하여</p>
-        <p>오히려 입냄새가 증가하며 최소 10회 이상 헹궈내야 합니다</p>
-      </div>
-    </div>
+    <Row>
+      <h3>나쁜 성분 빼기</h3>
+      <p>SLS(합성계면활성제, 인공L-멘톨, 파라벤, 트리클로산, 인공색소,</p>
+      <p>벤조산, 인공방부제, 광물성오일, CMTI-MIT(가습기살균제 성분)</p>
+      <OverlayTrigger trigger={['hover', 'focus']} placement="bottom" overlay={popoverHoverFocus}>
+        <p>합성계면활성제란
+          <img className="question-mark" src={questionMark} alt="#" />
+        </p>
+      </OverlayTrigger>
+    </Row>
+
+    <h3>좋은 성분 더하기</h3>
+    <Row className="good-stuff">
+      <OverlayTrigger placement="bottom" overlay={tooltip1}>
+        <Col className="good-stuff-column" xs={12} sm={2} md={2}>
+          <img src={leaf} alt="#" />
+          <h5>식물성계면활성제</h5>
+        </Col>
+      </OverlayTrigger>
+      <Col className="good-stuff-column" xs={12} sm={1} md={1}>
+        <img src={plus} alt="#" />
+      </Col>
+      <OverlayTrigger placement="bottom" overlay={tooltip2}>
+        <Col className="good-stuff-column" xs={12} sm={2} md={2}>
+          <img src={smile} alt="#" />
+          <h5>코엔자임 Q10</h5>
+        </Col>
+      </OverlayTrigger>
+      <Col className="good-stuff-column" xs={12} sm={1} md={1}>
+        <img src={plus} alt="#" />
+      </Col>
+      <OverlayTrigger placement="bottom" overlay={tooltip3}>
+        <Col className="good-stuff-column" xs={12} sm={2} md={2}>
+          <img src={teeth} alt="#" />
+          <h5>하이드록시아파타이트</h5>
+        </Col>
+      </OverlayTrigger>
+      <Col className="good-stuff-column" xs={12} sm={1} md={1}>
+        <img src={plus} alt="#" />
+      </Col>
+      <OverlayTrigger placement="bottom" overlay={tooltip4}>
+        <Col className="good-stuff-column" xs={12} sm={2} md={2}>
+          <img src={flower} alt="#" />
+          <h5>유기농 에코서트 인증</h5>
+        </Col>
+      </OverlayTrigger>
+    </Row>
   </div>
 );
 
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -8,9 +8,6 @@ import './Products.css';
 const Products = () => (
   <div className="product-container">
     <Row>
-      <Col id="product-titlebox" xs={12} sm={12} md={12}>
-        <p className="products"><Link to="productDetail">Products</Link></p>
-      </Col>
       <Row>
         <Col id="product-imagebox" xs={12} sm={12} md={8}>
           <img src={productImage} role="presentation" />
@@ -21,11 +18,13 @@ const Products = () => (
               <h2>좋은치약 smile</h2>
             </Col>
             <Col className="product-quote" xs={12} sm={12} md={12}>
-              {'"칫솔질 후에도 과일 맛을 그대로 즐길 수 있다?"'}
+              {'"칫솔질 후에도 과일 맛을'}<br className="breakline" />
+              {'그대로 즐길 수 있다?"'}
             </Col>
           </Col>
           <Col className="product-det1" xs={12} sm={12} md={12}>
-            요리사 소믈리에 바리스타 등 미각을 보호해 주는 치약
+            {'요리사 소믈리에 바리스타 등'}<br className="breakline" />
+            {'미각을 보호해 주는 치약'}
           </Col>
           <Col xs={12} sm={12} md={12}>
             <Link to="purchase" className="go-to-purchase">
@@ -47,11 +46,13 @@ const Products = () => (
             <h2>좋은치약 care</h2>
           </Col>
           <Col className="product-quote" xs={12} sm={12} md={12}>
-            {'"입안이 아파서 칫솔질을 할 수가 없어요"'}
+            {'"입안이 아파서 칫솔질을'}<br className="breakline" />
+            {'할 수가 없어요"'}
           </Col>
         </Col>
         <Col className="product-det1" xs={12} sm={12} md={12}>
-          어린이, 임산부, 노인 등 연약한 입안을 보호해 주는 치약
+          {'어린이, 임산부, 노인 등'}<br className="breakline" />
+          {'연약한 입안을 보호해 주는 치약'}
         </Col>
         <Col xs={12} sm={12} md={12}>
           <Link to="go-to-purchase" className="go-to-purchase">
